4171bb20e4324dad397ff79869e5aafe39059b97
Harshdentsu <harsh.dubey@dentsu.com>
2025-07-19 01:49:53 +0530

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -36,7 +36,7 @@ const Login = () => {
     }
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:8000/api/login", {
+     const response = await fetch("https://wheely-backend-cmdzb7awfucmc6c0.australiaeast-01.azurewebsites.net/api/login", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
